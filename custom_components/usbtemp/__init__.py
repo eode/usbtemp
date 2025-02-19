86b177954c9c64898a436d3f898154a495bfc303
Brian Ealdwine <brian@iron-moon.net>
2025-02-19 16:41:23 -0500

    (real) initial commit

---
+++ custom_components/usbtemp/__init__.py
@@ -0,0 +1,7 @@
+"""An integration for USBTEMP devices
+
+USBTEMP devices can be acquired at https://usbtemp.com
+
+I am unaffiliated with usbtemp, but they're a great, accurate,
+low-cost usb temperature sensor that uses a genuine DS18B20 sensor.
+"""
---
+++ custom_components/usbtemp/sensor.py
@@ -0,0 +1,94 @@
+"""Platform for sensor integration."""
+from __future__ import annotations
+
+# stdlib
+from pathlib import Path
+import re
+from time import time
+
+# 3rd party
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import UnitOfTemperature
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+# project imports
+from . import usbtemp
+
+
+KNOWN_DEVICES = (
+    ('067b', '23a3'),
+    )
+
+
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None
+) -> None:
+    """Set up the sensor platform."""
+    add_entities(find_devices())
+
+
+def find_devices():
+    # Look up devices by idVendor and idProduct using sysfs
+    def _is_usb(path):
+        return any(re.match(r'usb\d+', x) for x in path.parts)
+
+    devices = Path('/sys/devices').glob('**/idVendor')
+    devices = [dev.parent for dev in devices if _is_usb(dev)]
+
+    uids = set()
+    for vendor, product in KNOWN_DEVICES:
+        for dev in devices:
+            if vendor != (dev / 'idVendor').read_text().strip():
+                continue
+            if product != (dev / 'idProduct').read_text().strip():
+                continue
+            # get the serial number
+            serial = dev / 'serial'
+            assert serial.exists()  # no serial number present for this device
+            serial = serial.read_text().strip()
+            assert serial.exists()  # blank serial number on this device
+            # we only need the serial number.
+            uids.add(serial.read_text().strip())
+
+    # Look up a link to the specific ttyusbXX file
+    uid_path_map = {}
+    for path in Path("/dev/serial/by-id").glob('*'):
+        for uid in uids:
+            if uid in path.name:
+                uid_path_map[uid] = path
+    return [USBTempSensor(dev, uid) for (uid, dev) in uid_path_map.values()]
+
+
+class USBTempSensor(SensorEntity):
+    def __init__(self, device, uid, **kwargs):
+        self._attr_name = "USBTemp Thermometer"
+        self._attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
+        self._attr_device_class = SensorDeviceClass.TEMPERATURE
+        self._attr_state_class = SensorStateClass.MEASUREMENT
+        self._attr_unique_id = uid
+
+        self.linux_device = device
+        self.read_interval = 15
+        self._usbtemp_next_read = 0
+        self._usbtemp_thermometer = usbtemp.Thermometer(str(device))
+        self._usbtemp_thermometer.Open()
+
+        super().__init__(**kwargs)
+
+    def update(self) -> None:
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        if time() > self._usbtemp_next_read:
+            self._attr_native_value = self._usbtemp_thermometer.Temperature()
+            self._usbtemp_next_read = time() + self.read_interval
